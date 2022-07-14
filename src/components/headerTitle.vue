3266a85c3fcf00c5603306e057592e56cac34dd4
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-14 16:41:20 +0200

    add hero.vue and headerTitle.vue on app.vue

---
+++ src/components/headerTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hero-title">
+    <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'headerTitle',
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.hero-title{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50% , -50%);
+}
+.hero-title h1{
+  font-size: 100px;
+  color: white;
+  text-shadow: 5px 5px 10px red;
+}
+</style>
---
+++ src/components/hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hero-img">
+    <img src="https://camo.githubusercontent.com/94f8b20e660ebb3b1f4a84cd00efd53e26400469ac65c2de130396d0ebc0101a/68747470733a2f2f73332d75732d776573742d322e616d617a6f6e6177732e636f6d2f732e6364706e2e696f2f32383936332f7675652d736e69707065742d6865726f2e676966" alt="vue hero">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'heroImg',
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.hero-img img{
+    position: relative;
+    width: 100%;
+}
+</style>
